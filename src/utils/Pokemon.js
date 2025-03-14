8868fac3f3fe2e842ec2fa2223262d71783dc10a
andmana <iam.andmana@gmail.com>
2025-03-14 11:24:03 +0700

    Add fetch pokemon API

---
+++ src/utils/Pokemon.js
@@ -0,0 +1,41 @@
+class Pokemon {
+    constructor(id, name, imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.imgUrl = imgUrl;
+    }
+}
+
+const fetchRandomPokemon = async (count) => {
+    const uniquePokemonIds = new Set(); // Use Set for faster lookup of unique IDs
+    const pokemonList = [];
+
+    while (pokemonList.length < count) {
+        const id = Math.floor(Math.random() * 1025) + 1;
+
+        // Skip if ID is already in the set
+        if (uniquePokemonIds.has(id)) continue;
+
+        try {
+            const response = await fetch(
+                `https://pokeapi.co/api/v2/pokemon/${id}`
+            );
+            const data = await response.json(); // Await to get the parsed response
+
+            // Create a new Pokemon object and add it to the list
+            const pokemon = {
+                id: data.id,
+                name: data.name,
+                imageUrl: data.sprites.front_default,
+            };
+
+            // Add ID to the set and Pokémon to the list
+            uniquePokemonIds.add(id);
+            pokemonList.push(pokemon);
+        } catch (error) {
+            console.log("Error fetching Pokémon:", error.message);
+        }
+    }
+
+    return pokemonList; // Return the list of fetched Pokémon
+};
