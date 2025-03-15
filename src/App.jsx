e8911433f2038e68008c10c9978bd0e163cd3c63
andmana <iam.andmana@gmail.com>
2025-03-15 23:25:34 +0700

    Add musics and update music handle

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect, useRef } from "react";
 import { Howl } from "howler";
 import "./styles/app.scss";
 import Nav from "./components/Nav";
-import mainBgm from "./assets/musics/Ending.mp3";
 import Guidance from "./components/Guidance";
 import FirstLoad from "./components/FirstLoad";
 import Footer from "./components/Footer";
@@ -11,36 +10,76 @@ import Difficulty from "./components/Difficulty";
 import GamePlay from "./components/GamePlay";
 import Results from "./components/Results";
 
+import introBGM from "./assets/musics/intro-bgm.mp3";
+import difficultyBGM from "./assets/musics/difficulty-bgm.mp3";
+import gameplayBGM from "./assets/musics/gameplay-bgm.mp3";
+import winBGM from "./assets/musics/win-bgm.mp3";
+import loseBGM from "./assets/musics/lose-bgm.mp3";
+import landingBGM from "./assets/musics/landing-bgm.mp3";
+
+/* 
+    0. Landing
+    1. Intro
+    2. Set Difficulty
+    3. WIN
+    4. LOSE
+*/
+
+const bgms = {
+    99: landingBGM,
+    0: introBGM,
+    1: difficultyBGM,
+    2: gameplayBGM,
+    3: loseBGM,
+    4: winBGM,
+};
+
 function App() {
     const [state, setState] = useState(0);
     const [isFirstLoad, setFirstLoad] = useState(true);
     const [isGuideOpen, setIsGuideOpen] = useState(false);
 
-    const [bgm, setBgm] = useState(mainBgm);
+    const [bgm, setBgm] = useState(landingBGM);
     const [isMusicPlaying, setIsMusicPlaying] = useState(false);
     const soundRef = useRef(null);
     const hasMusicStarted = useRef(false);
 
-    const [difficulty, setDifficulty] = useState("easy");
+    const [difficulty, setDifficulty] = useState("hard");
 
     const handleSetState = (val) => {
         setState(val);
+        setBgm(bgms[val]);
     };
 
     const handleDifficulty = ({ target }) => {
         const diff = target.dataset.diff;
         setDifficulty(diff);
+        setBgm(bgms[2]);
         setState(2);
     };
 
     // Change bgm
     useEffect(() => {
+        if (soundRef.current) {
+            soundRef.current.stop();
+            soundRef.current.unload();
+        }
+
         soundRef.current = new Howl({
             src: [bgm],
             html5: true,
             loop: true,
             volume: 1,
         });
+
+        if (isMusicPlaying) {
+            soundRef.current.play();
+        }
+
+        return () => {
+            soundRef.current.stop();
+            soundRef.current.unload();
+        };
     }, [bgm]);
 
     // play / pause music
@@ -60,7 +99,7 @@ function App() {
             setFirstLoad(false);
 
             if (!hasMusicStarted.current) {
-                // setIsMusicPlaying(true);
+                setIsMusicPlaying(true);
                 hasMusicStarted.current = true;
             }
         }, 3500);
